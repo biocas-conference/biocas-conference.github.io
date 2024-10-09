41145e4e9f7727ee8707cf9522ace9522d72a965
zhangyuhang9 <343766376@qq.com>
2024-10-09 14:43:32 +0800

    update 2022 phoho

---
+++ utils/insert_photos.py
@@ -0,0 +1,53 @@
+import os
+
+
+
+
+search_root = "/home/yhzhang/workspace/biocas-conference.github.io"
+year_path = "photos/2022"
+search_path = os.path.join(search_root, year_path)
+
+
+
+
+
+filelist = []
+
+for root, dirs, files in os.walk(search_path):
+    for file in files:
+        if file.endswith(".jpg"):
+            filepath = os.path.join(year_path, file)
+            #print (filepath)
+            filelist.append(filepath)
+
+
+string = ""
+
+for i, each in enumerate(filelist):
+    if i % 4 == 0:
+        string += """
+<div class="flex flex-row pt-6 gap-4">\n
+        """
+
+    string += """
+    <div class="basis-1/4">
+        <img
+            class="rounded-t-lg"
+            src="{}"
+            alt=""
+            />
+    </div>
+    """.format(each)
+
+    if i % 4 == 3:
+        string += "\n</div>\n\n"
+    
+# 如果总计不是4的倍数，补齐
+if len(filelist) % 4 != 0:
+    string += "\n</div>"
+
+with open("tmp.html", "w") as f:
+    f.write(string)
+
+
+            
